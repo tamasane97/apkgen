8ef545aaf00977a2c8d758033a28b13b1947ba72
tushar masane <tamasane@gmail.com>
2020-05-17 18:25:04 +0530

    initial

---
+++ src/main/java/web/rundeckTrigger.java
@@ -0,0 +1,61 @@
+package web;
+
+import java.io.*;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.util.*;
+import org.rundeck.api.*;
+import org.rundeck.api.domain.*;
+import org.rundeck.api.domain.RundeckExecution.ExecutionStatus;
+
+@WebServlet
+public class rundeckTrigger extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final CloseableHttpClient httpClient = HttpClients.createDefault();
+	
+	private void triggerMyJob(String appUrl, String appName) {
+		RundeckClient rundeck = RundeckClient.builder().url("http://206.189.134.237:4440/").login("admin", "admin1234").build();
+		RundeckJob job = rundeck.getJob("debf006b-4bc5-4934-bdde-9b26eec54145");
+		RundeckExecution exec = rundeck.runJob(RunJobBuilder.builder()
+				.setOptions(new OptionsBuilder().addOption("APPURL", appUrl).addOption("APPNAME", appName).toProperties())
+				.setJobId(job.getId())
+				.build(), 10L, TimeUnit.SECONDS);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		String urlLink = req.getParameter("urlLink");
+		String appName = req.getParameter("appName");
+		String[] appUrl = urlLink.trim().split("\\.");
+		try {
+			String FILE_URL = "http://142.93.210.19:8000/"+appName+appUrl[1]+".apk";
+	        InputStream in = new URL( FILE_URL ).openStream();
+	        response.setContentType("application/vnd.android.package-archive");
+	        String headerKey = "Content-Disposition";
+	        String headerValue = String.format("attachment; filename=\"%s\"", appName+".apk");
+	        response.setHeader(headerKey, headerValue);        
+	        IOUtils.copy(in, response.getOutputStream());   
+		}
+		catch(Exception e) {
+			this.triggerMyJob(urlLink, appName);
+			String FILE_URL = "http://142.93.210.19:8000/"+appName+appUrl[1]+".apk";
+	        InputStream in = new URL( FILE_URL ).openStream();
+	        response.setContentType("application/vnd.android.package-archive");
+	        String headerKey = "Content-Disposition";
+	        String headerValue = String.format("attachment; filename=\"%s\"", appName+".apk");
+	        response.setHeader(headerKey, headerValue);        
+	        IOUtils.copy(in, response.getOutputStream());
+		}        
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+function validateURL(){
+	var str = document.getElementsByName("urlLink")[0].value;
+	 var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  console.log(!!pattern.test(str));
+}
+
+function validateFile(){
+	var myfile = document.getElementsByName("myfile")[0].value;
+	console.log(myfile);
+}
