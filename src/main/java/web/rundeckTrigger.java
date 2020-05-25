a49678428e12d0cc022e161d61250d2201d3e715
vaibhav026sharma@gmail.com <vaibhav026sharma@gmail.com>
2020-05-25 22:22:13 +0530

    Added RabbitMq

--- src/main/java/web/rundeckTrigger.java
+++ src/main/java/web/rundeckTrigger.java
@@ -8,12 +8,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.*;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.*;
 import org.apache.http.util.*;
+import org.json.simple.JSONObject;
 import org.rundeck.api.*;
 import org.rundeck.api.domain.*;
 import org.rundeck.api.domain.RundeckExecution.ExecutionStatus;
@@ -22,40 +26,40 @@ import org.rundeck.api.domain.RundeckExecution.ExecutionStatus;
 public class rundeckTrigger extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final CloseableHttpClient httpClient = HttpClients.createDefault();
-	
-	private void triggerMyJob(String appUrl, String appName) {
-		RundeckClient rundeck = RundeckClient.builder().url("http://206.189.134.237:4440/").login("admin", "admin1234").build();
-		RundeckJob job = rundeck.getJob("debf006b-4bc5-4934-bdde-9b26eec54145");
-		RundeckExecution exec = rundeck.runJob(RunJobBuilder.builder()
-				.setOptions(new OptionsBuilder().addOption("APPURL", appUrl).addOption("APPNAME", appName).toProperties())
-				.setJobId(job.getId())
-				.build(), 10L, TimeUnit.SECONDS);
-	}
+
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
 		String urlLink = req.getParameter("urlLink");
 		String appName = req.getParameter("appName");
-		String[] appUrl = urlLink.replaceAll("/", "").trim().split("\\.");
-		try {
-			String FILE_URL = "http://142.93.210.19:8000/"+appName+appUrl[1]+".apk";
-	        InputStream in = new URL( FILE_URL ).openStream();
-	        response.setContentType("application/vnd.android.package-archive");
-	        String headerKey = "Content-Disposition";
-	        String headerValue = String.format("attachment; filename=\"%s\"", appName+".apk");
-	        response.setHeader(headerKey, headerValue);        
-	        IOUtils.copy(in, response.getOutputStream());   
+		String email = req.getParameter("Email");
+		ConnectionFactory factory=new ConnectionFactory();
+
+		try
+		{
+			factory.setHost("159.65.158.218");
+			factory.setPort(5672);
+			factory.setUsername("admin");
+			factory.setPassword("admin");
+			Connection connection=factory.newConnection();
+			Channel channel=connection.createChannel();
+			channel.queueDeclare("apkGenQueue",false,false,false,null);
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("urlLink",urlLink);
+			jsonObject.put("appName",appName);
+			jsonObject.put("Email",email);
+			System.out.println(urlLink);
+			System.out.println(appName);
+			System.out.println(email);
+			channel.basicPublish("","apkGenQueue",false,null,jsonObject.toString().getBytes());
+			System.out.println("msg sent");
 		}
-		catch(Exception e) {
-			this.triggerMyJob(urlLink, appName);
-			String FILE_URL = "http://142.93.210.19:8000/"+appName+appUrl[1]+".apk";
-	        InputStream in = new URL( FILE_URL ).openStream();
-	        response.setContentType("application/vnd.android.package-archive");
-	        String headerKey = "Content-Disposition";
-	        String headerValue = String.format("attachment; filename=\"%s\"", appName+".apk");
-	        response.setHeader(headerKey, headerValue);        
-	        IOUtils.copy(in, response.getOutputStream());
-		}        
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		PrintWriter out = response.getWriter();
+		out.println("Your request is being processed. Your app will be generated within an hour and sent on your email. ");
     }
 
 }
