32eaef88c7717c5a8f9f7e0ff3c7f2e379c303a8
tushar <tamasane@gmail.com>
2020-05-29 02:15:51 +0530

    [updated] response page on request

--- src/main/java/web/rundeckTrigger.java
+++ src/main/java/web/rundeckTrigger.java
@@ -48,7 +48,66 @@ public class rundeckTrigger extends HttpServlet {
 			System.out.println(e);
 		}
 		PrintWriter out = response.getWriter();
-		out.println("Your request is being processed. Your app will be generated within an hour and sent on your email. ");
+		out.println(request_recvd_msg);
     }
+
+
+    final String request_recvd_msg = "<!doctype html>\n" +
+			"<html lang=\"en\">\n" +
+			"<head>\n" +
+			"    <meta charset=\"utf-8\">\n" +
+			"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n" +
+			"    <meta name=\"description\" content=\"\">\n" +
+			"    <meta name=\"author\" content=\"\">\n" +
+			"    <link rel=\"icon\" href=\"icon/favicon.ico\">\n" +
+			"\n" +
+			"    <title>APKGen :: Android App Generator</title>\n" +
+			"\n" +
+			"\n" +
+			"    <link href=\"style.css\" rel=\"stylesheet\">\n" +
+			"</head>\n" +
+			"\n" +
+			"<body class=\"text-center\">\n" +
+			"\n" +
+			"\n" +
+			"<header>\n" +
+			"    <nav class=\"navbar navbar-expand-md navbar-dark fixed-top bg-dark\">\n" +
+			"        <img src=\"icon/favicon.ico\" width=\"60\" height=\"60\"/>\n" +
+			"        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarCollapse\" aria-controls=\"navbarCollapse\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+			"            <span class=\"navbar-toggler-icon\"></span>\n" +
+			"        </button>\n" +
+			"        <div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\n" +
+			"            <ul class=\"navbar-nav mr-auto\">\n" +
+			"                <li class=\"nav-item\">\n" +
+			"                    <a class=\"nav-link\" href=\"index.html\">Home <span class=\"sr-only\">(current)</span></a>\n" +
+			"                </li>\n" +
+			"                <li class=\"nav-item\">\n" +
+			"                    <a class=\"nav-link\" onclick=\"window.open('https://github.com/tamasane/apkgen');\">Github</a>\n" +
+			"                </li>\n" +
+			"                <li class=\"nav-item\">\n" +
+			"                    <a class=\"nav-link\" href=\"#\">About Us</a>\n" +
+			"                </li>\n" +
+			"            </ul>\n" +
+			"        </div>\n" +
+			"    </nav>\n" +
+			"</header>\n" +
+			"\n" +
+			"<div class=\"jumbotron\">\n" +
+			"    <div class=\"container\">\n" +
+			"        <h1 class=\"display-3\">Request Recieved!</h1>\n" +
+			"        <p><br/><br/>We have recieved your request. Sit back, have a Coffee and <br/>check your Email, while our magic elves stitch your application!<br/><br/> Have a nice day &#128522; ! <br/><br/></p>\n" +
+			"        <p><a class=\"btn btn-primary btn-lg\" onclick=\"window.open('http://apkgen.digital','_self','');\" role=\"button\">Make another App? Click here &raquo;</a></p>\n" +
+			"    </div>\n" +
+			"    <p class=\"mt-5 mb-3 text-muted\">APK Gen &copy; 2020</p>\n" +
+			"</div>\n" +
+			"\n" +
+			"<script src=\"index.jsp\" type=\"text/javascript\"> </script>\n" +
+			"<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n" +
+			"<script src=\"popper.min.js\"></script>\n" +
+			"<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\n" +
+			"<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n" +
+			"\n" +
+			"</body>\n" +
+			"</html>\n";
 
 }
